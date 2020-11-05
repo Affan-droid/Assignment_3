ba5177852daf9d680a72cd31de28a544256cfd2c
Affan Javaid <affanraog11@gmail.com>
2020-11-05 23:49:39 +0500

    logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <p>
-        <h1>Affan Assignment</h1>
-        <h2>CI and CD through workflows</h2>
-        <h3>Deployment on heroku through workflow</h3>
+    <p className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>Affan Assignment</h1>
+      <h2>CI and CD through workflows</h2>
+      <h3>Deployment on heroku through workflow</h3>
     </p>
     </div>
   );
